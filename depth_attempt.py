cf9f07af6ab36625c9afb98ba8df8f6fc6adb729
Maxli <maxli@maxlicatenby.org>
2021-10-16 21:22:28 -0400

    Stab at depth processing, giving up for now

---
+++ depth_attempt.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+from matplotlib import pylab as plt
+
+if __name__ == "__main__":
+    left_name  = "C:\\Users\\Maxli\\Pictures\\Camera Roll\\WIN_20211016_21_10_09_Pro.jpg"
+    right_name = "C:\\Users\\Maxli\\Pictures\\Camera Roll\\WIN_20211016_21_10_17_Pro.jpg"
+
+    left  = cv2.imread(left_name,0)
+    right = cv2.imread(right_name,0)
+
+    cv2.imshow("window", left)
+    cv2.waitKey(0)
+    cv2.imshow("window", right)
+    cv2.waitKey(0)
+
+    stereo = cv2.StereoBM_create(numDisparities=96, blockSize=15)
+    disparity = stereo.compute(left, right)
+    #plt.figure(figsize=(20, 10))
+    #plt.imshow(disparity, 'disparity')
+    #plt.xticks([])
+    #plt.yticks([])
+    cv2.imshow("window", disparity)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
